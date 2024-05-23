9841f238faab7999623b551130e3defc97fe8d36
haducluonghnk2023 <haducluongdz@gmail.com>
2024-05-23 11:39:15 +0700

    add

--- src/components/Hw9.tsx
+++ src/components/Hw9.tsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import React from "react";
 
 export default function Hw9() {
   return (
-    <div>Hw9</div>
-  )
+    <div>
+      <h1>B9</h1>
+      <nav className="w-[250px] h-[auto] bg-slate-700">
+        <div>
+          <span className="material-symbols-outlined text-4xl p-1">
+            drag_pan
+          </span>
+          <hr />
+        </div>
+        <div>
+          <ul className="pl-2">
+            <li className="pt-2  p-3 ">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">home</span>
+                Dashboard
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">post_add</span>
+                Post
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">movie</span>
+                Media
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">description</span>
+                Pages
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">chat</span>
+                Comments
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">grid_view</span>
+                Appearance
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">local_mall</span>
+                Plugins
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">group</span>
+                Users
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">settings</span>
+                Settings
+              </a>
+            </li>
+            <li className="pt-2 p-3">
+              <a href="" className="  flex items-center text-white gap-6">
+                <span className="material-symbols-outlined">build</span>
+                Tools
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="mt-8 flex justify-around gap-11">
+          <div className="flex justify-center items-center">
+            <span className="material-symbols-outlined">dark_mode</span>
+            Darkmode
+          </div>
+          <span className="material-symbols-outlined text-3xl">toggle_off</span>
+        </div>
+        <hr />
+        <div className="flex justify-center items-center h-12">
+          <div className="w-[270px] h-8 bg-red-500 rounded-lg flex items-center ">
+            <span className="material-symbols-outlined ml-6">logout</span>
+            Logout
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
